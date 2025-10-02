7a5f3f4f82d60c98f418664f584d06cde67f553a
Yasin Atagun <yasinatagun@gmail.com>
2025-10-02 22:00:32 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cloud-animation
+
+Flutter Cloud Animation On Background - A beautiful animated background with floating clouds.
---
+++ android/app/src/main/kotlin/com/codentro/animation_work/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.codentro.animation_work
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
